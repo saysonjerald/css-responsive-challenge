0b0dd5eeeb1ea830897c2fdc4f8f3e66449041f8
Jerald Sayson <saysonjerald@gmail.com>
2022-10-04 22:50:38 +0800

    Working with resposiveness

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.nav');
+// const navToggle = document.querySelector('.nav-toggle');
+// const nav = document.querySelector('.nav');
 
 
-navToggle.addEventListener('click', () => {
-    nav.classList.toggle('nav--visible');
+// navToggle.addEventListener('click', () => {
+//     nav.classList.toggle('nav--visible');
+// })
+
+const menuButton = document.getElementById('menu-button');
+const nav = document.querySelector('.link');
+
+menuButton.addEventListener('click', () => {
+    nav.classList.toggle('--visible');
 })
